fed24e7cc47d2dcdafb15a0e19119f8fa32a99fa
Konrad <w.konrad60@gmail.com>
2017-09-24 10:42:55 +0200

    skrypcik do zadania 8.6

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-var name = prompt('Enter your name');
-// komentarz 1
-alert('Hello, ' + name);
-/*komenatrz 2*/
-console.log('Hello, ' + name);
+var a = prompt("Podaj a: ");
+var b = prompt("Podaj b: ");
+var	value = (a*a) + (2 * a * b) - (b * b);
+
+if(value > 0) {
+	alert("wynik dodatni: " + value);
+	console.log("wynik dodatni: " + value);
+}
+else if (value == 0) {
+	alert("wynik zerowy: " + value);
+	console.log("wynik zerowy: " + value);
+}
+else{
+	alert("wynik ujemny: " + value);
+	console.log("wynik ujemny: " + value);	
+}
