d29826a96e5866cfa0858847d6a2fa01a4016597
Konrad <w.konrad60@gmail.com>
2017-09-24 08:58:33 +0200

    Dodany skrypt na pole trojkata

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var name = prompt('Enter your name');
-// komentarz 1
-alert('Hello, ' + name);
-/*komenatrz 2*/
-console.log('Hello, ' + name);
+var a = prompt('podaj bok');
+var h = prompt('podaj wysokosc');
+var triangleArea = a*h/2;
+alert('pole to: ' + triangleArea);
+console.log('pole to: ' + triangleArea);
